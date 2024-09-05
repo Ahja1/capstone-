47e54026a555cf5360b708417b66fde8161a5abb
Ahja1 <ahjadias1@gmail.com>
2024-09-05 12:07:55 -0400

    commit

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 
 function CreatePage() {
-    // Your component logic
     return (
         <div>
             <h1>Create New Product</h1>
-            {/* Your form and other components */}
         </div>
     );
 }
